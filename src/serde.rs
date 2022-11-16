ca5a48e134a6556da706522592973601d594c0ed
Sprite Tong <spritetong@gmail.com>
2022-11-16 17:56:52 +0800

    Add ser_x_optional_bytes & de_x_optional_bytes.

--- src/serde.rs
+++ src/serde.rs
@@ -168,10 +168,24 @@ where
 struct DeBytesVisitor<T: From<Vec<u8>>>(PhantomData<T>);
 
 impl<'de, T: From<Vec<u8>>> de::Visitor<'de> for DeBytesVisitor<T> {
-    type Value = T;
+    type Value = Option<T>;
 
     fn expecting(&self, formatter: &mut fmt::Formatter) -> fmt::Result {
         formatter.write_str("a BASE64 encoded string")
+    }
+
+    fn visit_none<E>(self) -> Result<Self::Value, E>
+    where
+        E: de::Error,
+    {
+        Ok(None)
+    }
+
+    fn visit_some<D>(self, deserializer: D) -> Result<Self::Value, D::Error>
+    where
+        D: de::Deserializer<'de>,
+    {
+        deserializer.deserialize_str(self)
     }
 
     fn visit_str<E>(self, v: &str) -> Result<Self::Value, E>
@@ -179,7 +193,7 @@ impl<'de, T: From<Vec<u8>>> de::Visitor<'de> for DeBytesVisitor<T> {
         E: de::Error,
     {
         base64::decode(v.as_bytes())
-            .map(|x| x.into())
+            .map(|x| Some(x.into()))
             .map_err(|_| E::invalid_value(de::Unexpected::Str(v), &self))
     }
 }
@@ -193,19 +207,46 @@ where
     serializer.serialize_str(&base64::encode(this.as_ref()))
 }
 
-/// Function to deserializing a BASE64 encoded string to a **`Vec<u8>`**,
-/// the separator is ',', ';', or '\n'
+/// Function to deserializing a BASE64 encoded string to a `&[u8]` slice.
 pub fn de_x_bytes<'de, T, D>(deserializer: D) -> Result<T, D::Error>
 where
     T: From<Vec<u8>>,
     D: de::Deserializer<'de>,
 {
-    deserializer.deserialize_str(DeBytesVisitor::<T>(PhantomData))
+    deserializer
+        .deserialize_str(DeBytesVisitor::<T>(PhantomData))
+        .map(|x| x.unwrap())
+}
+
+/// Function to serializing an optional `&[u8]` slice to an optional BASE64 encoded string.
+pub fn ser_x_optional_bytes<T, S>(this: &Option<T>, serializer: S) -> Result<S::Ok, S::Error>
+where
+    T: AsRef<[u8]>,
+    S: ser::Serializer,
+{
+    match this {
+        Some(x) => serializer.serialize_str(&base64::encode(x.as_ref())),
+        None => serializer.serialize_none(),
+    }
+}
+
+/// Function to deserializing an optional BASE64 encoded string to a an optional `&[u8]` slice.
+pub fn de_x_optional_bytes<'de, T, D>(deserializer: D) -> Result<Option<T>, D::Error>
+where
+    T: From<Vec<u8>>,
+    D: de::Deserializer<'de>,
+{
+    deserializer.deserialize_option(DeBytesVisitor::<T>(PhantomData))
 }
 
 pub mod serde_x_bytes {
     pub use super::de_x_bytes as deserialize;
     pub use super::ser_x_bytes as serialize;
+}
+
+pub mod serde_x_optional_bytes {
+    pub use super::de_x_optional_bytes as deserialize;
+    pub use super::ser_x_optional_bytes as serialize;
 }
 
 ////////////////////////////////////////////////////////////////////////////////
@@ -289,4 +330,40 @@ pub mod serde_x_strings {
 pub mod serde_x_vec {
     pub use super::de_x_vec as deserialize;
     pub use super::ser_x_vec as serialize;
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use serde::{Deserialize, Serialize};
+
+    #[derive(Clone, Debug, Serialize, Deserialize, PartialEq, Eq)]
+    struct Person {
+        #[serde(with = "serde_x_bytes")]
+        name: Vec<u8>,
+        #[serde(with = "serde_x_optional_bytes")]
+        nickname: Option<Vec<u8>>,
+    }
+
+    #[test]
+    fn test_serde() {
+        let mut a = Person {
+            name: vec![1],
+            nickname: Some(vec![2]),
+        };
+        let s = serde_json::to_string(&a).unwrap();
+        println!("{}", &s);
+        let b: Person = serde_json::from_str(&s).unwrap();
+        assert_eq!(&a, &b);
+
+        assert!(serde_json::from_str::<Person>(r#"{"name":null,"nickname":"Ag=="}"#).is_err());
+        assert!(serde_json::from_str::<Person>(r#"{"name":1,"nickname":"Ag=="}"#).is_err());
+        assert!(serde_json::from_str::<Person>(r#"{"name":"","nickname":"Ag=="}"#).is_ok());
+
+        a.nickname = None;
+        let s = serde_json::to_string(&a).unwrap();
+        println!("{}", &s);
+        let b: Person = serde_json::from_str(&s).unwrap();
+        assert_eq!(&a, &b);
+    }
 }
