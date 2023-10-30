52065b5eac60ad72aec85364973aec05ec04b8ad
Sprite Tong <spritetong@gmail.com>
2023-10-30 14:30:14 +0800

    Modify the feature "common".

--- src/serde.rs
+++ src/serde.rs
@@ -40,7 +40,7 @@ impl<'de, T: Default + PrimInt + FromPrimitive + FromStr> de::Visitor<'de> for D
     }
 }
 
-/// Function to deserializing a string to an **`integer`**
+/// Function to deserializing an integer or a string to an **`integer`**
 #[cfg(feature = "num")]
 pub fn de_x_num<'de, D, T>(deserializer: D) -> Result<T, D::Error>
 where
@@ -63,7 +63,7 @@ impl<'de, T: Default + Float + FromPrimitive + FromStr> de::Visitor<'de> for DeF
     type Value = T;
 
     fn expecting(&self, formatter: &mut fmt::Formatter) -> fmt::Result {
-        formatter.write_str("an integer or a string")
+        formatter.write_str("a float number or a string")
     }
 
     fn visit_f64<E>(self, v: f64) -> Result<Self::Value, E>
@@ -96,7 +96,7 @@ impl<'de, T: Default + Float + FromPrimitive + FromStr> de::Visitor<'de> for DeF
     }
 }
 
-/// Function to deserializing a string to an **`float`**
+/// Function to deserializing a float number or a string to a **`float`**
 #[cfg(feature = "num")]
 pub fn de_x_float<'de, D, T>(deserializer: D) -> Result<T, D::Error>
 where
@@ -155,7 +155,8 @@ impl<'de> de::Visitor<'de> for DeBoolVisitor {
     }
 }
 
-/// Function to deserializing a string to a **`number`**
+/// Function to deserializing a boolean value, an integer (0/1) or
+/// a string ("true"/"false") to a **`bool`**
 pub fn de_x_bool<'de, D>(deserializer: D) -> Result<bool, D::Error>
 where
     D: de::Deserializer<'de>,
@@ -184,7 +185,7 @@ impl<'de, T: FromStr> de::Visitor<'de> for DeStringsVisitor<T> {
     }
 }
 
-/// Function to serializing a **`Vec<String>`** to a simple string, the separator is ','
+/// Function to serializing a **`Vec<String>`** to a simple string with the separator ", ".
 pub fn ser_x_strings<S>(this: &[String], serializer: S) -> Result<S::Ok, S::Error>
 where
     S: ser::Serializer,
@@ -192,8 +193,8 @@ where
     serializer.serialize_str(this.join(", ").as_str())
 }
 
-/// Function to deserializing a simple string to a **`Vec<String>`**,
-/// the separator is ',', ';', or '\n'
+/// Function to deserializing a simple string to a **`Vec<String>`**
+/// with separators of ',', ';', or '\n'.
 pub fn de_x_strings<'de, D>(deserializer: D) -> Result<Vec<String>, D::Error>
 where
     D: de::Deserializer<'de>,
@@ -202,7 +203,7 @@ where
 }
 
 /// Module to serialize and deserialize a **`Vec<String>`** to/from a simple string,
-/// the separator is ',', ';', or '\n'**`Vec<String>`**
+/// it can recognize separators of ',', ';', or '\n'.
 pub mod serde_x_strings {
     pub use super::de_x_strings as deserialize;
     pub use super::ser_x_strings as serialize;
